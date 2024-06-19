a6ce0c61c206252180f45944f689d04e2ab21cfc
jesusce <jesusce9413@gmail.com>
2024-06-19 11:13:08 -0500

    vista crud con CBV:update, delete

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,8 +1,8 @@
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-from django.urls import reverse 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 
 
 # Create your views here.
@@ -18,6 +18,16 @@ class PageDetailView(DetailView):
 class PageCreate(CreateView):
     model = Page 
     fields = ["title", 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+    
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ["title", 'content', 'order']
+    template_name_suffix = "_update_form"
     
     def get_success_url(self):
-        return reverse('pages:pages')
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+    
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy("pages:pages")
