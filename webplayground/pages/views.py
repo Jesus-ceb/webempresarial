7364b5ce960d76330350f156cf336ae1e554c039
jesusce <jesusce9413@gmail.com>
2024-06-19 11:47:59 -0500

    vista crud con CBV:seguridad con Mixin

--- webplayground/pages/views.py
+++ webplayground/pages/views.py
@@ -1,12 +1,24 @@
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from .models import Page
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 
 
 # Create your views here.
 
+class StaffRequiredMixin(object):
+    """
+    Este mixin permitira que te valides como usuario para realizar las operacion CRUD
+    """ 
+    def dispatch(self, request,*args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login')) 
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+    
 class PagesListView(ListView):
     
     model = Page
@@ -15,12 +27,13 @@ class PageDetailView(DetailView):
     
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page 
     fields = ["title", 'content', 'order']
     success_url = reverse_lazy('pages:pages')
     
-class PageUpdate(UpdateView):
+    
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     fields = ["title", 'content', 'order']
     template_name_suffix = "_update_form"
@@ -28,6 +41,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
     
-class PageDelete(DeleteView):
+    
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy("pages:pages")
